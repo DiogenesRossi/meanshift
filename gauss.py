a2ff98e8d77ef587ead869aa58e3a9c1b2b73b37
DiogenesRossi <diogenesrossi@hotmail.com>
2019-09-12 12:38:35 -0300

    Usando gauss como peso

--- gauss.py
+++ gauss.py
@@ -24,7 +24,7 @@ is the Euclidean distance given by
 ||x_i - x_j||=((x_i1-x_j1)^2 + (x_i2-x_j2)^2 + ... + (x_iD-x_jD)^2)^.5
 '''
 def euclidian_distance(p, q):
-    return math.hypot(p[0]-q[0],p[1]-q[1])
+    return math.hypot(p[0]-q[0], p[1]-q[1])
 
 '''
 The Gaussian kernel is defined as
@@ -37,32 +37,30 @@ def calc_gaussian(p, S, h):
     return sum([gaussian_kernel(p, q, h) for q in S])
 
 
-
+# retorna o modulo da distancia euclidiana no formato de ponto flutuante
 def _euclidian_distance(p, q):
-    return math.fabs(p-q)
+    return math.fabs(p-q)  
 
 def _gaussian_kernel(p, q, r):
     return math.exp(-_euclidian_distance(p, q)**2/r**2)
 
 
-
-
-
-b=(5,5)
-#S=[(1,1),(5,5),(99,99)]
-S=[(4,1), (5,3), (7,1)]
+b=(3,3)
+S=[(2,2), (4,4), (5,5)]
+#S=[(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7)]
 h=1
-#print (calc_gaussian(b, S, h))
-
+print (calc_gaussian(b, S, h))
+print "- - - - - - "
+print S
+print "point: ", b[0]
 o=0
+m=0
 for q in S:
 	w = _gaussian_kernel(b[0], q[0], h)
-	d = _euclidian_distance(b[0], q[0])
-	o += w*d
-	print (q[0],w,d, w*d)
-print(o)
+	o += w
+	m += w*q[0]
 
-print ( (4+5+7)/3)
+print m/o
 
 
 
